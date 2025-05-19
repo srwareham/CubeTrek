c3f1be82ef69c479c0af6060318350009dd421ce
srwareham <ebriouscoding@gmail.com>
2025-05-18 21:48:49 -0700

    update deprecated spring security call

--- src/main/java/com/cubetrek/SecurityConfig.java
+++ src/main/java/com/cubetrek/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(csrf -> csrf.disable())
                 .authorizeRequests(authorize -> authorize
                         .requestMatchers("/",
                                 "/css/**",
